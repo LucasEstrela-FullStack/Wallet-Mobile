677efe676e8aeada4b3715875f4be18b352d557c
LucasEstrela-FullStack <l1abreuestrela@gmail.com>
2025-08-04 02:16:30 -0400

    Authentication Email

--- app/index.jsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function Index() {
-  return (
-    <View>
-      <Text style={{color: "blue" }}>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
-}
